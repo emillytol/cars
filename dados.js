019e2a57e592d8fb5837509af9d8c5493795ac98
emillytol <emitoledo2006@gmail.com>
2024-09-06 12:16:10 -0400

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+        titulo: "Bmw 320i",
+        descricao: "A BMW 320i é um sedã esportivo que combina desempenho, elegância e tecnologia. Conhecida por sua agilidade e manobrabilidade, ela oferece uma experiência de condução dinâmica e prazerosa.",
+        link: "https://www.karvi.com.br/blog/bmw-320i-precos-versoes-e-tudo-sobre-o-modelo/"
+    },
+    {
+        titulo: "Audi R8",
+        descricao: "A Audi R8 é mais do que apenas um carro, é uma experiência de condução intensa e emocionante. Desenvolvida para os entusiastas de alta performance, a R8 combina design futurista, engenharia de ponta e um motor poderoso para oferecer uma experiência de direção única.",
+        link: "ttps://www.audi.com.br/br/web/pt/models/r8/r8-coupe-v10-performance-quattro.html"
+    },
+    {
+        titulo: "Civic G10 Exl",
+        descricao: "Conheça as versões Sport, EX e EXL do Civic Geração 10, que traz o Conceito Honda em sofisticação, segurança e incrível dirigibilidade com performance econforto inigualáveis.",
+        link: "https://www.comprecar.com.br/revista/avaliacao-honda-civic-exl-2017-g10?srsltid=AfmBOop7pEI65wqxzFXJzQRMne8HPUnnpfyi2nNk0I2MbFwNEVxKH0XM"
+    }
+];
